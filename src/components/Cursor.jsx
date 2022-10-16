35c4c358f1b0eef3cc74fc944507ee677b72408c
UziStacks <terel.phillips23@gmail.com>
2022-10-15 20:17:23 -0400

    -changes to dynamic cursor color

--- src/components/Cursor.jsx
+++ src/components/Cursor.jsx
@@ -6,11 +6,12 @@ import { BurgerMenuContext } from "../context/burgerMenu.context";
 const Cursor = () => {
   const { burgerState } = useContext(BurgerMenuContext);
   const { variants, cursorVariant } = useContext(CursorContext);
+  console.log(burgerState);
 
   return (
     <motion.div
       className={`${
-        burgerState === "close" ? "bg-white" : "bg-black"
+        burgerState === "open" ? "bg-black" : "bg-white"
       } fixed h-[20px] w-[20px] top-0 left-0 rounded-full pointer-events-none z-[105]`}
       variants={variants}
       animate={cursorVariant}
